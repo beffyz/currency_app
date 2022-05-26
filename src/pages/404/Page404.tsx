aa5fcd9bfb144dca1f76445e79f08375cf0cad74
Bogdans Zabela <bogdanz@inbox.lv>
2022-05-26 03:16:49 +0300

    Initial commit

---
+++ src/pages/404/Page404.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './page404.module.scss';
+
+const Page404 = () => (
+  <div className={styles.error_page}>
+    <h1>Page Not Found</h1>
+  </div>
+);
+
+export default Page404;
